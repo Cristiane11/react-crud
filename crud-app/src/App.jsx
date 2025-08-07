a82edc2463a9f109cc6fd5622278e11a080acbc1
Cristiane11 <bjwmcd@yahoo.com>
2025-08-07 12:21:24 -0400

    update

--- crud-app/src/App.jsx
+++ crud-app/src/App.jsx
@@ -1,24 +1,22 @@
-import { BrowserRouter, Routes , Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
 import Home from './Home'
 import Create from './Create'
 import Update from './Update'
 import Read from './Read'
-
-
 import './App.css'
 
 function App() {
 
 
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Router path="/" element ={<Home/>}></Router>
-        <Router path="/create" element ={<Create/>}></Router>
-        <Router path="/update" element ={<Update/>}></Router>
-        <Router path="/read" element ={<Read/>}></Router>
+        <Route path="/" element ={<Home/>}/>
+        <Route path="/create" element ={<Create/>}/>
+        <Route path="/update/:id" element ={<Update/>}/>
+        <Route path="/read/:id" element ={<Read/>}/>
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
--- crud-app/src/Home.jsx
+++ crud-app/src/Home.jsx
@@ -1,8 +1,25 @@
+//import axios from "axios";
+import {useEffect} from 'react';
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Container from "react-bootstrap/Container";
+//import Button from 'react-bootstrap/Button';
+
+
 function Home(){
+    useEffect(()=>{
+        axios.get('http://localhost:3000/users')
+
+    },[])
     return(
-        <div>
-        Home
+        <div className='Container'>
+                <div className='d-flex flex-column justify-content-center align-items-center vh-100 '>
+                    <h1>List of Users</h1>
+                    <div className='w-75 rounded bg-white boder shadow p-4'>
+
+                    </div>
+       
+                    </div>
         </div>
     )
 }
